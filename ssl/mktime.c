eb2e9649e59072be132961d425a8eb50739bc14d
Ted Ying <yingted@gmail.com>
2015-08-24 01:00:32 -0400

    remove pebble stuff

--- ssl/mktime.c
+++ ssl/mktime.c
@@ -39,6 +39,7 @@ static long ymd_to_scalar (unsigned yr, unsigned mo, unsigned day) {
   return scalar;
 }
 
+#if 0
 static void scalar_to_ymd (long scalar,
 			   unsigned *pyr,
 			   unsigned *pmo,
@@ -62,6 +63,7 @@ static void scalar_to_ymd (long scalar,
   *pday = n - months_to_days(*pmo);
   return;
 }
+#endif
 
 time_t os_port_mktime (struct tm *timeptr) {
   time_t tt;
@@ -78,75 +80,4 @@ time_t os_port_mktime (struct tm *timeptr) {
     tt = tt * 60 + timeptr->tm_sec;
   }
   return tt;
-}
-
-/* dow - written by Paul Edwards, 1993-01-31 */
-/* Released to the Public Domain */
-/* This routine will work over the range 1-01-01 to 32767-12-31.
-   It assumes the current calendar system has always been in
-   place in that time.  If you pass 0 or negative years, then
-   it produces results on the assumption that there is a year
-   0.  It should always produce a value in the range of 0..6
-   if a valid month and day have been passed, no matter what
-   the year is.  However, it has not been tested for negative
-   years, because the results are meaningless anyway.  It is
-   mainly to stop people playing silly buggers and causing
-   the macro to crash on negative years. */
-
-#define dow(y,m,d) \
-  ((((((m)+9)%12+1)<<4)%27 + (d) + 1 + \
-  ((y)%400+400) + ((y)%400+400)/4 - ((y)%400+400)/100 + \
-  (((m)<=2) ? ( \
-  (((((y)%4)==0) && (((y)%100)!=0)) || (((y)%400)==0)) \
-  ? 5 : 6) : 0)) % 7)
-
-/*
-  Note: Pebble doesn't think about timezones. So we pass a time_t to
-  gmtime that's already been offset for the desired TZ, and get a struct tm
-  object back in that TZ. I don't know how seedy actual developers will
-  think that is.
-*/
-static struct tm tms;
-
-struct tm *pgmtime (const time_t *timer) {
-  unsigned yr, mo, da;
-  unsigned long secs, days;
-
-  days = *timer / (60L*60*24);
-  secs = *timer % (60L*60*24);
-  scalar_to_ymd(days + ymd_to_scalar(1970, 1, 1), &yr, &mo, &da);
-  tms.tm_year = yr - 1900;
-  tms.tm_mon = mo - 1;
-  tms.tm_mday = da;
-  tms.tm_yday = (int)(ymd_to_scalar(tms.tm_year + 1900, mo, da)
-		      - ymd_to_scalar(tms.tm_year + 1900, 1, 1));
-  tms.tm_wday = dow(tms.tm_year + 1900, mo, da);
-  tms.tm_isdst = -1;
-  tms.tm_sec = (int)(secs % 60);
-  secs /= 60;
-  tms.tm_min = (int)(secs % 60);
-  secs /= 60;
-  tms.tm_hour = (int)secs;
-  return &tms;
-}
-
-char *pstrtok(char *s1, const char *s2) {
-  static char *old = NULL;
-  char *p;
-  size_t len;
-  size_t remain;
-
-  if (s1 != NULL) old = s1;
-  if (old == NULL) return (NULL);
-  p = old;
-  len = strspn(p, s2);
-  remain = strlen(p);
-  if (remain <= len) { old = NULL; return (NULL); }
-  p += len;
-  len = strcspn(p, s2);
-  remain = strlen(p);
-  if (remain <= len) { old = NULL; return (p); }
-  *(p + len) = '\0';
-  old = p + len + 1;
-  return(p);
 }
